2aded1f57c34125b41e86878c723a72c276c46ca
Vinicius Martins <vinicius.goncalves@gft.com>
2022-11-11 16:07:03 -0300

    feat: Estrutura da aplicação - finalizando sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from "phosphor-react";
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -9,11 +10,19 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https:github.com/viniciusmartins1.png"
+        />
+
         <strong>Vinicius Martins</strong>
         <span>Web Develop</span>
 
         <footer>
-          <a href="#">Editar seu perfil</a>
+          <a href="#">
+            <PencilLine size={20} />
+            Editar seu perfil
+          </a>
         </footer>
       </div>
     </aside>
