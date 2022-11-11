d07054557bdb306be200e9d8b8f9e79377242073
Vinicius Martins <vinicius.goncalves@gft.com>
2022-11-11 15:32:33 -0300

    feat: Estrutura da aplicação - component sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { Header } from "./components/Header";
 
+import styles from "./App.module.css";
+
 import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main></main>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1554306274-f23873d9a26c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=50"
+      />
+
+      <div className={styles.profile}>
+        <strong>Vinicius Martins</strong>
+        <span>Web Develop</span>
+
+        <footer>
+          <a href="#">Editar seu perfil</a>
+        </footer>
+      </div>
+    </aside>
+  );
+}
