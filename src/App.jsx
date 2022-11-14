c146899109452eab00e9e585cdccd891abf539b0
Vinicius Martins <vinicius.goncalves@gft.com>
2022-11-14 17:16:44 -0300

    feat: Os motores do React: funcionalidade de comentarios e dados por propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,45 @@ import styles from "./App.module.css";
 
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/viniciusmartins1.png",
+      name: "Vinicius Martins",
+      role: "Desenvolvedor Front-end",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          " Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-11-01 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          " Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-11-13 20:00:00"),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -14,14 +53,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Vinicius Martins"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur ea soluta obcaecati aperiam explicabo molestias libero, perspiciatis ad quam consectetur voluptatum, eligendi quaerat culpa! Totam dolorem voluptate harum facilis ut!"
-          />
-          <Post
-            author="Vinicius Martins"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur ea soluta obcaecati aperiam explicabo molestias libero, perspiciatis ad quam consectetur voluptatum, eligendi quaerat culpa! Totam dolorem voluptate harum facilis ut!"
-          />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
