887ce595f026160aca273ec59edcfb97221015f5
Vinicius Martins <vinicius.goncalves@gft.com>
2022-11-14 12:18:51 -0300

    feat: Estrutura da aplicacao: componente Avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
@@ -6,10 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/viniciusmartins1.png"
-          />
+          <Avatar src="https://github.com/diego3g.png" />
           <div className={styles.authorInfo}>
             <strong>Vinicius Martins</strong>
             <span>Web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -10,10 +11,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https:github.com/viniciusmartins1.png"
-        />
+        <Avatar src="https:github.com/diego3g.png" />
 
         <strong>Vinicius Martins</strong>
         <span>Web Develop</span>
